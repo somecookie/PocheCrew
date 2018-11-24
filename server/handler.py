0ebcf4a7e59620ced9575eb3329c2cde6f3da7d4
Diana Petrescu <diana.petrescu@epfl.ch>
2018-11-24 23:15:16 +0100

    Fix the server

--- server/handler.py
+++ server/handler.py
@@ -1,11 +1,9 @@
 # coding: utf-8
-from http.server import SimpleHTTPRequestHandler, BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import urlparse
+from http.server import SimpleHTTPRequestHandler
 import hashlib
-import os
 import base64
-import re
 import json
+import binascii
 
 class Image_handler(SimpleHTTPRequestHandler):
     def do_POST(self):
@@ -15,9 +13,7 @@ class Image_handler(SimpleHTTPRequestHandler):
         post_data = self.rfile.read(content_length)
         
         image_json = json.loads(post_data)
-        image_data = image_json['image'].replace('\\\\','')
-        
-        print(image_data[-20:])
+        image_data = binascii.a2b_base64(image_json['image'])
         #create a unique file_name
         m = hashlib.sha1(post_data)
         file_name = m.hexdigest()
@@ -26,20 +22,6 @@ class Image_handler(SimpleHTTPRequestHandler):
         path_name = "./images/{}.png".format(file_name)
         
         with open(path_name, "wb") as file:
-            file.write(base64.decodestring(image_data))
+            file.write(image_data)
 
         print("DONE")
-
-
-def decode_base64(data, altchars=b'+/'):
-    """Decode base64, padding being optional.
-
-    :param data: Base64 data as an ASCII byte string
-    :returns: The decoded byte string.
-
-    """
-    #data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)  # normalize
-    missing_padding = len(data) % 4
-    if missing_padding:
-        data += b'='* (4 - missing_padding)
-    return base64.b64decode(data)
