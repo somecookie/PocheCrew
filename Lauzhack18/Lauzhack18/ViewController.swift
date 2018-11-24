2f05f2de0399558d5ea9e23ba16adccf5ebd042a
Lionel Pellier <lionel.pellier@epfl.ch>
2018-11-24 13:23:35 +0100

    dataset

---
+++ Lauzhack18/Lauzhack18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lauzhack18
+//
+//  Created by Lionel Pellier on 24/11/2018.
+//  Copyright © 2018 Lionel Pellier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
